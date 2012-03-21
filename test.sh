3c50845c412f90a1592d1d6ca82c96fa5e7e7401
Nuno Job <nunojobpinto@gmail.com>
2012-03-21 00:08:02 +0000

    [test] fix problems with travis stacks

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-node test/specify.js | sed 's/.\[[0-9][0-9]m//g' > test/all.log
+node test/specify.js | sed 's/.\[[0-9][0-9]m//g' | sed 's/"at.*\(.*\)//' \
+  > test/all.log
 node test/specify.js specify#throws specify#cascading_sync \
-  | sed 's/.\[[0-9][0-9]m//g' >  test/filters.log
+  | sed 's/.\[[0-9][0-9]m//g' | sed 's/"at.*\(.*\)//' >  test/filters.log
 diff test/all.log test/fixtures/all.txt
 if [ $? -eq 0 ]; then
   diff test/filters.log test/fixtures/filters.txt
